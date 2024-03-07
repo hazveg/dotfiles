78bf664e2236e1255a00836d42a82d829a7e05d0
hazveg <133221043+hazveg@users.noreply.github.com>
2024-03-07 11:12:56 +0100

    added a check for konsole symlink, user.js and stylus.json
    user.js and stylus.json are just copied to the home directory, because getting
    the necessary paths from firefox is essentially impossible.

--- lnx_install.sh
+++ lnx_install.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 
+echo "DO NOT RUN THIS WITH SUDO"
 echo "Overwrite existing configuration files? (y/N)"
 read confirmation
 
 if [ "$confirmation" = "y" ] || [ "$confirmation" = "Y" ]
 then
+	echo "Deleting .bashrc"
+	rm ~/.bashrc
+
 	echo "Creating symlink for ~/.bashrc"
 	ln -s ~/.dotfiles/.bashrc ~/.bashrc
 
@@ -14,9 +18,20 @@ then
 	echo "Creating symlink for ~/.config/nvim"
 	ln -s ~/.dotfiles/nvim ~/.config/nvim
 
+	echo "Checking wether konsole dir exists already"
+	if [ -d ~/.local/share/konsole ]; then
+		echo "~/.local/share/konsole exists, deleting now"
+		rm -r ~/.local/share/konsole
+	fi
+
 	echo "Creating symlink for ~/.local/share/konsole"
 	ln -s ~/.dotfiles/konsole ~/.local/share/konsole
 
+	echo "Copying user.js to ~/user.js"
+	cp ~/.dotfiles/user.js ~/user.js
+
+	echo "Copying stylus.json to ~/stylus.json"
+	cp ~/.dotfiles/stylus.json ~/stylus.json
 else
 	echo "Installation cancelled by user"
 fi
