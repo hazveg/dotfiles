5bb7907dd921fd7985437b37c5fb89f78043b46c
hazveg <133221043+hazveg@users.noreply.github.com>
2024-05-14 08:26:27 +0200

    added tmux, edited zathura TODO: finish script

--- lnx_install.sh
+++ lnx_install.sh
@@ -1,6 +1,36 @@
 #! /bin/bash
 
 echo "DO NOT RUN THIS WITH SUDO"
+
+echo "Would you like to customize the installation? (y/N)"
+read customize
+
+if [ "$customize" = "y" ] || [ "$customize" = "Y" ]
+then
+	to_remove=()
+	while :
+	do
+		echo "Listed are all the components that will be installed."
+		echo "To remove any of these, enter a number."
+		# echo "If you wish to remove multiple at once; you can enter a list seperated by commas e.g. [1, 5, 6]."
+		echo "Once you are satisfied with your selection, enter 'commit' to begin installation."
+
+		echo "[1] ~/.bashrc"
+		echo "[2] ~/.gitconfig"
+		echo "[3] ~/.config/nvim"
+		echo "[4] ~/.local/share/konsole"
+		echo "[5] ~/.config/zathura"
+		echo "[6] ~/.config/qimgv"
+		echo "[7] ~/.tmux.conf"
+		echo "[8] Firefox Settings"
+
+		read selection
+		to_remove+=($selection)
+
+		echo ${to_remove[*]}
+	done
+fi
+
 echo "Overwrite existing configuration files? (y/N)"
 read confirmation
 
@@ -8,7 +38,6 @@ if [ "$confirmation" = "y" ] || [ "$confirmation" = "Y" ]
 then
 	echo "Deleting .bashrc"
 	rm ~/.bashrc
-
 	echo "Creating symlink for ~/.bashrc"
 	ln -s ~/.dotfiles/.bashrc ~/.bashrc
 
@@ -18,11 +47,13 @@ then
 	echo "Creating symlink for ~/.config/nvim"
 	ln -s ~/.dotfiles/nvim ~/.config/nvim
 
-	echo "Checking wether konsole dir exists already"
+	echo "Checking whether konsole dir exists already"
 	if [ -d ~/.local/share/konsole ]; then
 		echo "~/.local/share/konsole exists, deleting now"
 		rm -r ~/.local/share/konsole
 	fi
+	echo "Creating symlink for ~/.local/share/konsole"
+	ln -s ~/.dotfiles/konsole ~/.local/share/konsole
 
 	echo "Creating symlink for ~.config/zathura"
 	ln -s ~/.dotfiles/zathura ~/.config/zathura
@@ -30,8 +61,8 @@ then
 	echo "Creating symlink for ~.config/qimgv"
 	ln -s ~/.dotfiles/qimgv ~/.config/qimgv
 
-	echo "Creating symlink for ~/.local/share/konsole"
-	ln -s ~/.dotfiles/konsole ~/.local/share/konsole
+	echo "Creating symlink for ~/.tmux.conf"
+	ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 
 	echo "Copying user.js to ~/user.js"
 	cp ~/.dotfiles/user.js ~/user.js
