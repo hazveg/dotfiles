7ec5f36bee740a62eb6c618f07152a169fc38410
hazveg <133221043+hazveg@users.noreply.github.com>
2024-06-21 10:47:57 +0200

    removed windows stuff from master branch

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++
@@ -1,8 +0,0 @@
-Set-Alias -Name vim -Value "C:\Program Files\Neovim\bin\nvim.exe"
-
-function Prompt {
-    Write-Host "$ENV:UserName@$ENV:ComputerName" -NoNewLine -ForegroundColor Green
-	Write-Host ":" -NoNewLine
-	Write-Host "$(Get-Location)" -NoNewLine -ForegroundColor Blue
-	"$ "
-}
