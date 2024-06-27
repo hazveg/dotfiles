0ad864271e68a3422f0d4dd990ebbeda680ab13b
hazveg <133221043+hazveg@users.noreply.github.com>
2024-06-27 13:24:01 +0200

    profile now translates USERPROFILE to ~

--- powershell/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ powershell/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -3,6 +3,8 @@ Set-Alias -Name vim -Value "C:\Program Files\Neovim\bin\nvim.exe"
 function Prompt {
     Write-Host "$ENV:UserName@$ENV:ComputerName" -NoNewLine -ForegroundColor Green
 	Write-Host ":" -NoNewLine
-	Write-Host "$(Get-Location)" -NoNewLine -ForegroundColor Blue
+	$location = Get-location
+	$location = if ("$(Get-Location)" -eq "$Env:USERPROFILE") { "~" } else { $(Get-Location) }
+	Write-Host "$location" -NoNewLine -ForegroundColor Blue
 	"$ "
 }
