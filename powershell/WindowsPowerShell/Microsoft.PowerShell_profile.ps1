5e20a9ec0a867e620b35ff0e836fdb2b9dc06749
hazveg <133221043+hazveg@users.noreply.github.com>
2024-07-28 13:10:27 +0200

    updated prompt, simplified ~

--- powershell/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ powershell/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,9 +2,7 @@ Set-Alias -Name vim -Value "C:\Program Files\Neovim\bin\nvim.exe"
 
 function Prompt {
     Write-Host "$ENV:UserName@$ENV:ComputerName" -NoNewLine -ForegroundColor Green
-	Write-Host ":" -NoNewLine
-	$location = Get-location
-	$location = if ("$(Get-Location)" -eq "$Env:USERPROFILE") { "~" } else { $(Get-Location) }
-	Write-Host "$location" -NoNewLine -ForegroundColor Blue
+	Write-Host " " -NoNewLine
+	Write-Host "$(Get-Location)".replace("C:\Users\aaron", "~") -ForegroundColor Blue
 	"$ "
 }
